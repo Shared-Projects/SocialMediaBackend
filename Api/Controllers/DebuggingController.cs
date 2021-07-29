e82b08b4ddca40f5946faa80f1113081d17bb361
Andrei Bucur <andrei.bucur@cst.ro>
2021-07-29 21:37:00 +0300

    [UPDATE] Changed the controller name and done some small fixes

--- Api/Controllers/DebuggingController.cs
+++ Api/Controllers/DebuggingController.cs
@@ -9,26 +9,30 @@ namespace Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class DebuggingController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<DebuggingController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public DebuggingController(ILogger<DebuggingController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<object> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {})
+            return Enumerable.Range(1, 5).Select(index => new 
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
             .ToArray();
         }
     }
