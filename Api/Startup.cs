75b3e6ff81d623f5b08deeb6579aac016fd172dc
radu radu <raducadar@yahoo.com>
2021-08-08 08:53:54 +0300

    [FEAT]Swagger config

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Api
 {
@@ -18,8 +19,35 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Name = "Authorize",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+
+                var securityRequirement = new OpenApiSecurityRequirement
+                    {
+                        { securitySchema, new[] { "Bearer" } }
+                    };
+
+                c.AddSecurityRequirement(securityRequirement);
+            });
+
+
             services.AddControllers();
-            services.AddSwaggerGen();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -30,6 +58,7 @@ namespace Api
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api");
             });
+
             app.UseRouting();
 
             app.UseAuthorization();
